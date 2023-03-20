b354b6a430b1e8268fc89622a4854dbdb10af37a
Hong Jungmin <jungmin898@naver.com>
2023-03-20 16:04:36 +0900

    3/20 수업 완료

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,20 +1,55 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.dto.ResponseDTO;
+import com.example.demo.dto.TestRequestBodyDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("test") // url 지정하는 어노테이션
 public class TestController {
     @GetMapping("/testGetMapping")  // localhost:8080/test/testGetMapping
-    public String testContoller() {
+    public String testController() {
         return "Hello World, 홍정민";
     }
 
     @GetMapping("/{id}")
     public String testControllerWithPathVariables(@PathVariable(required = false) int id) {
         return "Hello World! ID " + id;
+    }
+
+    @GetMapping("/testRequestParam")
+    public String testControllerRequestParam(@RequestParam(required = false) int id) {
+        return "Hello World! ID " + id;
+    }
+
+    @GetMapping("/testRequestBody")
+    public String testControllerRequestBody(@RequestBody TestRequestBodyDTO testRequestBodyDTO) {
+        return "Hello World! ID : " + testRequestBodyDTO.getId() + " Message : " + testRequestBodyDTO.getMessage();
+    }
+
+    @GetMapping("/testResponseBody")
+    public ResponseDTO<String> testControllerResponseBody() {
+        List<String> list = new ArrayList<>();
+        list.add("Hello World! I'm ResponseDTO");
+        list.add("hjm");
+        list.add("hjm22");
+        list.add("hjm333");
+        ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+        return response;
+    }
+
+    @GetMapping("/testResponseEntity")
+    public ResponseEntity<?> testControllerResponseEntity() {
+        List<String> list = new ArrayList<>();
+        list.add("Hello World! I'm ResponseEntity");
+        list.add("hjm");
+        list.add("hjm22");
+        list.add("hjm333");
+        ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+        return ResponseEntity.badRequest().body(response);
     }
 }
